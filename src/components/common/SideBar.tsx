c69c44e71a7660e6d1f7683d0060acc7c43ffcf9
KIMDJ\vzmzm <azmzm@naver.com>
2025-08-20 21:52:33 +0900

    [dj][2025-08-20] 폰트, 사이드바 수정

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -7,129 +7,91 @@ import {useMainContentStore} from "../../store/MainContentStore.tsx";
 export default function SideBar() {
     const isDarkMode = useThemeStore((state) => state.isDarkMode);
     const toggleTheme = useThemeStore((state) => state.toggleTheme);
-    const {isPost, isAbout, setIsPost, setIsAbout} =useMainContentStore();
+    // const {isPost, setIsPost, isAbout, setIsAbout} =useMainContentStore();
+    const {isPost, setIsPost} =useMainContentStore();
 
     const handleLink = (link: string) => {
         window.open(link, '_blank');
     };
 
-    const handleAbout = () => {
-        if (isAbout) {
-            setIsAbout(false);
-            setIsPost(false);
-        } else {
-            setIsAbout(true);
-            setIsPost(false);
-        }
-    };
+    // const handleAbout = () => {
+    //     if (isAbout) {
+    //         setIsAbout(false);
+    //         setIsPost(false);
+    //     } else {
+    //         setIsAbout(true);
+    //         setIsPost(false);
+    //     }
+    // };
 
     return (
-        // <div className="min-w-56 lg:max-w-xs lg:mb-0">
-        //     <div
-        //         className="flex flex-col items-center gap-4
-        //         min-w-56 lg:max-w-xs lg:mb-0
-        //         fixed top-10 lg:left-5 xl:left-20"
-        //     >
-        //         <div
-        //             className="flex-center
-        //             lg:text-xl lg:mb-4
-        //             xl:text-xxl xl:mb-6"
-        //         >
-        //             <button onClick={handleAbout}>
-        //                 ddj82's blog
-        //             </button>
-        //         </div>
-        //         <div className="flex-center">
-        //             <button
-        //                 type="button"
-        //                 onClick={() => handleLink('https://github.com/ddj82/')}
-        //             >
-        //                 <FontAwesomeIcon icon={faGithub} className="w-6 h-6" />
-        //             </button>
-        //         </div>
-        //         <div className="flex-center">
-        //             <button
-        //                 type="button"
-        //                 onClick={toggleTheme}
-        //             >
-        //                 {isDarkMode ? <Moon className="w-6 h-6"/> : <Sun className="w-6 h-6"/>}
-        //             </button>
-        //         </div>
-        //         {isPost && (
-        //             <div className="flex-center post-arrowLeft-btn">
-        //                 <button
-        //                     type="button"
-        //                     onClick={() => setIsPost(false)}
-        //                 >
-        //                     <ArrowLeft className="w-7 h-7"/>
-        //                 </button>
-        //             </div>
-        //         )}
-        //     </div>
-        // </div>
-        <aside
-            className={`
-            /* 공통 */
-            row-start-1 lg:col-start-1
-            z-40 text-black dark:text-white
-    
-            /* 모바일/태블릿: 상단 헤더 */
-            sticky top-0
-            w-full
-            p-5 border-b
-            bg-[rgb(var(--bg-opacity,255)_/_var(--tw-bg-opacity,1))]/80
-            backdrop-blur-lg
-            flex items-center justify-between gap-3
-    
-            /* 데스크탑(lg+): 왼쪽 사이드 */
-            lg:static lg:top-auto lg:z-auto
-            lg:bg-transparent
-            lg:p-0 lg:border-none
-            lg:w-56 xl:w-64
-            lg:flex lg:flex-col lg:items-start lg:justify-start
-            `}
-        >
-            {/* 타이틀 (모바일: 좌측, 데스크탑: 상단) */}
+        <aside>
             <div
-                className="font-semibold tracking-tight
-                text-xl
-                lg:text-xxl lg:mb-6
-                "
+                className={`
+                /* 공통 */
+                row-start-1 lg:col-start-1
+                z-40 text-black dark:text-white
+        
+                /* 모바일/태블릿: 상단 헤더 */
+                sticky top-0
+                w-full
+                p-5 border-b
+                bg-[rgb(var(--bg-opacity,255)_/_var(--tw-bg-opacity,1))]/80
+                backdrop-blur-lg
+                flex items-center justify-between gap-3
+        
+                /* 데스크탑(lg+): 왼쪽 사이드 */
+                lg:top-10
+                lg:bg-transparent
+                lg:p-0 lg:border-none
+                lg:w-56 xl:w-64
+                lg:flex lg:flex-col lg:items-start lg:justify-start
+                `}
             >
-                <button onClick={handleAbout}>ddj82's blog</button>
-            </div>
-
-            {/* 아이콘 영역: 모바일은 가로, 데스크탑은 세로 정렬 */}
-            <div
-                className="flex items-center gap-4
-                lg:flex-col lg:items-start lg:gap-3
-                "
-            >
-                <button
-                    type="button"
-                    onClick={() => handleLink('https://github.com/ddj82/')}
-                    className="w-7 h-7"
+                {/* 타이틀 (모바일: 좌측, 데스크탑: 상단) */}
+                <div
+                    className="font-semibold tracking-tight
+                    text-lg
+                    md:text-xl
+                    lg:text-xxl lg:mb-6
+                    "
                 >
-                    <FontAwesomeIcon icon={faGithub} className="w-7 h-7" />
-                </button>
+                    {/*<button onClick={handleAbout}>ddj82's blog</button>*/}
+                    <span>ddj82's blog</span>
+                </div>
 
-                <button
-                    type="button"
-                    onClick={toggleTheme}
-                    className="w-7 h-7"
+                {/* 아이콘 영역: 모바일은 가로, 데스크탑은 세로 정렬 */}
+                <div
+                    className="flex items-center gap-4
+                    lg:flex-col lg:items-start lg:gap-3
+                    "
                 >
-                    {isDarkMode ? <Moon className="w-7 h-7" /> : <Sun className="w-7 h-7" />}
-                </button>
-
-                {isPost && (
                     <button
                         type="button"
-                        onClick={() => setIsPost(false)}
-                        className="lg:self-start w-7 h-7"
+                        onClick={() => handleLink('https://github.com/ddj82/')}
+                        className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"
                     >
-                        <ArrowLeft className="w-7 h-7" />
+                        <FontAwesomeIcon icon={faGithub} className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />
                     </button>
-                )}
+
+                    <button
+                        type="button"
+                        onClick={toggleTheme}
+                        className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"
+                    >
+                        {isDarkMode ? <Moon className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" /> : <Sun className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />}
+                    </button>
+
+                    {isPost && (
+                        <button
+                            type="button"
+                            onClick={() => setIsPost(false)}
+                            className="lg:self-start w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"
+                        >
+                            <ArrowLeft className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7" />
+                        </button>
+                    )}
+                </div>
             </div>
         </aside>
     );
